4a7f9c1eb1c382b23a158145557b95035c043118
lilith-avatar-linli <73171683+lilith-avatar-linli@users.noreply.github.com>
2020-11-25 10:23:02 +0800

    删除了多余的脚本

---
+++ code/['World']['Npc2']['NPCmgrModule'].ModuleScript.lua
@@ -0,0 +1,38 @@
+local NPCmgrModule = {}
+local NPC = script.Parent
+
+function NPCmgrModule:Run(behaviorJson)
+	print('------------------------------------------------')
+	local behaviorTree = B3.BehaviorTree.new()
+	local blackBoard = B3.Blackboard.new()
+	behaviorTree:load(behaviorJson, {})
+	print('------------------------------------------------')
+	
+	while (true) do
+		wait()
+		behaviorTree:tick(NPCmgrModule, blackBoard)
+	end
+end
+
+function NPCmgrModule:StatusCheck(B3,_node)
+	--print(_node.title)
+    if (_node.title == 'CanSeePlayer') then
+        return self:CanSeePlayer(B3)
+    end
+    return B3.SUCCESS
+end
+
+function NPCmgrModule:HeroAction(B3,_node)
+	print(NPC.Name..'看到了你')
+	return B3.SUCCESS
+end
+
+function NPCmgrModule:CanSeePlayer(B3)
+    if Vector3.Angle(NPC.Forward, localPlayer.Position - NPC.Position) < 45 then
+        return B3.SUCCESS
+    else
+        return B3.FAILURE
+    end
+end
+
+return NPCmgrModule
---
+++ code/['World']['Npc2']['NpcMainScript'].Script.lua
@@ -0,0 +1,98 @@
+local behaviorJson =
+[[
+{
+  "version": "0.3.0",
+  "scope": "tree",
+  "id": "298c3e26-e389-4354-aee6-0bc6db6fa551",
+  "title": "AISoldier",
+  "description": "",
+  "root": "e4ba5b3b-33bd-4355-8026-376a20ff121c",
+  "properties": {},
+  "nodes": {
+    "e4ba5b3b-33bd-4355-8026-376a20ff121c": {
+      "id": "e4ba5b3b-33bd-4355-8026-376a20ff121c",
+      "name": "Sequence",
+      "category": "composite",
+      "title": "Sequence",
+      "description": "",
+      "properties": {},
+      "display": {
+        "x": 132,
+        "y": 0
+      },
+      "children": [
+        "3edb35b2-c66a-4da8-b72a-70c2ab8ade68",
+        "22fbbc05-f5f9-4d06-848d-d77ff98d3bdc"
+      ]
+    },
+    "22fbbc05-f5f9-4d06-848d-d77ff98d3bdc": {
+      "id": "22fbbc05-f5f9-4d06-848d-d77ff98d3bdc",
+      "name": "HeroAction",
+      "category": "action",
+      "title": "Shot",
+      "description": "角色动作汇总，根据Title执行不同动作",
+      "properties": {},
+      "display": {
+        "x": 336,
+        "y": 48
+      }
+    },
+    "3edb35b2-c66a-4da8-b72a-70c2ab8ade68": {
+      "id": "3edb35b2-c66a-4da8-b72a-70c2ab8ade68",
+      "name": "StatusCheck",
+      "category": "condition",
+      "title": "CanSeePlayer",
+      "description": "角色状态检测，根据不同的Title执行不同检测",
+      "properties": {
+        "range": 90
+      },
+      "display": {
+        "x": 336,
+        "y": -36
+      }
+    }
+  },
+  "display": {
+    "camera_x": 492,
+    "camera_y": 380.5,
+    "camera_z": 1,
+    "x": 0,
+    "y": 0
+  },
+  "custom_nodes": [
+    {
+      "version": "0.3.0",
+      "scope": "node",
+      "name": "HeroAction",
+      "category": "action",
+      "title": "角色动作",
+      "description": "角色动作汇总，根据Title执行不同动作",
+      "properties": {}
+    },
+    {
+      "version": "0.3.0",
+      "scope": "node",
+      "name": "Print",
+      "category": "action",
+      "title": "打印",
+      "description": "打印一些信息",
+      "properties": {},
+      "parent": null
+    },
+    {
+      "version": "0.3.0",
+      "scope": "node",
+      "name": "StatusCheck",
+      "category": "condition",
+      "title": "角色状态",
+      "description": "角色状态检测，根据不同的Title执行不同检测",
+      "properties": {},
+      "parent": null
+    }
+  ],
+  "custom_folders": []
+}
+]]
+
+local NPC = require(script.Parent.NPCmgrModule)
+NPC:Run(behaviorJson)
